d81429a106a1da088d5663c268ed74ac914e8977
Tanusree Das <tithydas3@gmail.com>
2023-01-29 13:46:04 +0600

    paginate add

--- Components/Main.tsx
+++ Components/Main.tsx
@@ -4,13 +4,24 @@ import React from "react";
 import CharacterList from "./CharacterList";
 import EpisodeList from "./EpisodeList";
 import LocationList from "./LocationList";
-
+import ReactPaginate from "react-paginate";
+const handlePageClick = () => {
+	console.log("clicked");
+};
 export const Main = () => {
 	return (
 		<div>
 			<CharacterList />
 			<EpisodeList />
 			<LocationList />
+			<ReactPaginate
+				className="flex"
+				previousLabel="<"
+				pageCount={10}
+				nextLabel=">"
+				breakLabel={"..."}
+				onPageChange={handlePageClick}
+			/>
 		</div>
 	);
 };
